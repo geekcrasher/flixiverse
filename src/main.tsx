6c69f3cd85989a474e709e261ad404b254f09167
Paulo Lopez <geekcrasher@gmail.com>
2024-08-14 21:56:26 +0800

    feat: wrap the App component inside the provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { MovieDBContextProvider } from './context/MovieDBContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MovieDBContextProvider>
+      <App />
+    </MovieDBContextProvider>
   </React.StrictMode>,
 )
