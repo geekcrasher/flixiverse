95faa5e2f85cc49cb5ff47f99b2e7ae0d2ce84a3
Paulo Lopez <geekcrasher@gmail.com>
2024-08-14 21:59:27 +0800

    perf: implement the lazy loading for better performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,20 @@
-import { useEffect, useState } from "react"
-import { discoverMovies } from "./api/discover"
-import { type MovieList } from "./lib/types"
-import {
-  Card,
-  CardContent,
-} from "@/components/ui/card"
+import { lazy, Suspense } from "react"
 import Navbar from "./components/Navbar"
+import SkeletonLoading from "./components/Skeleton"
+
+const Movies = lazy(() => import('@/components/Movie/Discover'))
 
 function App() {
 
-  const [movies, setMovies] = useState<MovieList | null>(null)
-
-  useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const data = await discoverMovies()
-        setMovies(data)
-        console.log(data)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-
-    fetchMovies()
-  }, [])
-
-  console.log(movies)
-
   return (
     // 262c39
-    <main className="min-h-screen w-full flex flex-col bg-[#20252e]">
+    // 20252e
+    <main className="min-h-screen w-full flex flex-col bg-white">
       <Navbar />
-      <section className="flex items-center justify-center flex-wrap gap-4 mt-8">
-        {movies && movies.results.map(movie => (
-          <Card key={movie.id} className="overflow-hidden border-0 flex hover:cursor-pointer">
-            <CardContent className="p-0">
-              <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="" className="w-[200px] h-[275px]" />
-            </CardContent>
-          </Card>
-        ))}
+      <section className="flex items-center flex-wrap gap-4 mt-8">
+        <Suspense fallback={<SkeletonLoading />} >
+          <Movies />
+        </Suspense>
       </section>
     </main>
   )
