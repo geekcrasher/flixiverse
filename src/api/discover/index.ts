83fbc8306eadf7c7ae5deb26895eb248b29ec77c
Paulo Lopez <geekcrasher@gmail.com>
2024-08-14 21:57:43 +0800

    refactor: add more type safety and improve error handling

--- src/api/discover/index.ts
+++ src/api/discover/index.ts
@@ -1,13 +1,17 @@
-import { AxiosResponse } from "axios";
-import { api } from '@/api/api'
-import { type MovieList } from "@/lib/types"
+import { AxiosError, AxiosResponse } from "axios";
+import { api } from '@/api/api';
+import { type MovieList } from "@/lib/types";
 
-export const discoverMovies = async (): Promise<MovieList> => {
+export const discoverMovies = async (page: number = 1): Promise<MovieList> => {
   try {
-    const response: AxiosResponse<MovieList> = await api.get<MovieList>('/discover/movie')
-    return response.data
-  } catch (error) {
-    console.error('Error fetching movie data:', error);
-    throw error
+    const response: AxiosResponse<MovieList> = await api.get<MovieList>(`/discover/movie?page=${page}`);
+    return response.data;
+  } catch (error: unknown) {
+    if (error instanceof AxiosError) {
+      console.error('Error fetching movie data:', error.message);
+    } else {
+      console.error('Unexpected error:', error);
+    }
+    throw error;
   }
-}
+};
