e83950cf9ff89332f00eee5b03539b5fdc915f1f
Paulo Lopez <geekcrasher@gmail.com>
2024-08-16 23:51:54 +0800

    remove the previous element and import the Outlet component from react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import { lazy, Suspense } from "react"
-import Navbar from "./components/Navbar"
-import SkeletonLoading from "./components/Skeleton"
-
-const Movies = lazy(() => import('@/components/Movie/Discover'))
+import Header from "./components/Header"
+import { Outlet } from "react-router-dom"
 
 function App() {
-
   return (
     // 262c39
     // 20252e
     <main className="min-h-screen w-full flex flex-col bg-white">
-      <Navbar />
-      <section className="flex items-center flex-wrap gap-4 mt-8">
-        <Suspense fallback={<SkeletonLoading />} >
-          <Movies />
-        </Suspense>
-      </section>
+      <Header />
+      <Outlet />
     </main>
   )
 }
