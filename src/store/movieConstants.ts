b93802a68165532683c350b11df28ad6b20f76d2
Paulo Lopez <geekcrasher@gmail.com>
2024-08-24 23:09:00 +0800

    feat: set the initial value of the objects for movidb store

---
+++ src/store/movieConstants.ts
@@ -0,0 +1,41 @@
+export const initialSelectedMovie = {
+  id: 0,
+  original_language: '',
+  original_title: '',
+  overview: '',
+  popularity: 0,
+  poster_path: '',
+  release_date: '',
+  vote_average: 0,
+  vote_count: 0,
+};
+
+export const initialMovieCollections = {
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0,
+};
+
+export const initialFilteredDetails = {
+  credits: {
+    cast: [],
+  },
+  genres: {
+    name: '',
+  },
+  id: null,
+  original_title: '',
+  overview: '',
+  popularity: 0,
+  poster_path: '',
+  release_date: '',
+  revenue: 0,
+  reviews: [],
+  runtime: 0,
+  videos: {
+    results: [],
+  },
+  vote_average: 0,
+  vote_count: 0,
+};
