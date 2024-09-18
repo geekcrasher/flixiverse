540ecf0758808ab20060983bafafa1f2072c8793
Paulo Lopez <geekcrasher@gmail.com>
2024-09-18 17:07:32 +0800

    refactor: remove the unnecessary states and actions

--- src/store/useMovieDBStore.ts
+++ src/store/useMovieDBStore.ts
@@ -1,22 +1,14 @@
 import { create } from 'zustand';
 import { createJSONStorage, devtools, persist } from 'zustand/middleware';
-import {
-  type FilteredDetails,
-} from '@/lib/types';
-import {
-  initialFilteredDetails
-} from './movieConstants';
+import { type FilteredDetails } from '@/lib/types';
+import { initialFilteredDetails } from './movieConstants';
 
 type State = {
-  // selectedMovie: MovieCollections
-  // movieCollections: MovieList
   selectedMovieFilteredDetails: FilteredDetails
   searchMovieTitle: string
 }
 
 type Action = {
-  // setSelectedMovie: (value: MovieCollections) => void
-  // setMovieCollections: (value: MovieList) => void
   setSearchTitle: (title: string) => void
   setSelectedMovieFilteredDetails: (value: FilteredDetails) => void
 }
@@ -25,10 +17,6 @@ export const useMovieDBStore = create<State & Action>()(
   devtools(
     persist(
       (set) => ({
-        // selectedMovie: initialSelectedMovie,
-        // movieCollections: initialMovieCollections,
-        // setSelectedMovie: (value) => set({ selectedMovie: value }),
-        // setMovieCollections: (value) => set({ movieCollections: value }),
         searchMovieTitle: '',
         selectedMovieFilteredDetails: initialFilteredDetails,
         setSearchTitle: (title) => set({ searchMovieTitle: title }),
