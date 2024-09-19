dd0222cbfdfa612c067688ff9cd4880dd7618252
Paulo Lopez <geekcrasher@gmail.com>
2024-09-19 21:35:28 +0800

    component for rendering the movie base on the searched title

--- src/pages/SearchMovie/index.tsx
+++ src/pages/SearchMovie/index.tsx
@@ -1,10 +1,14 @@
 import { Suspense } from 'react';
 import SkeletonLoading from "@/components/Skeleton";
 import SearchMovieResults from "./SearchMovieResults";
+import SearchBar from '@/components/SearchBar';
 
 const SearchMovie = () => {
   return (
     <section className="flex items-center flex-wrap gap-4 mt-8">
+      <section className='w-full block md:hidden'>
+        <SearchBar />
+      </section>
       <Suspense fallback={<SkeletonLoading />}>
         <SearchMovieResults />
       </Suspense>
