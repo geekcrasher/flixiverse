8bed9f6ce98cad0d1d906b70debd0724e8fb1950
Paulo Lopez <geekcrasher@gmail.com>
2024-09-20 13:25:25 +0800

    refactor: add a type guards

--- src/hooks/useMovieFilteredDetails.ts
+++ src/hooks/useMovieFilteredDetails.ts
@@ -1,23 +1,44 @@
-import { useCallback, useEffect } from "react";
-import { useMovieDBStore } from "@/store/useMovieDBStore";
+import { useCallback, useEffect, useState } from "react";
 import { fetchFilteredMovieDetails } from "@/api/discover/movieDetails";
 import { FilteredDetails } from "@/lib/types";
+import { useParams } from "react-router-dom";
+import { initialFilteredDetails } from "@/constants/movieConstants";
 
-// Custom hook for movie details
-export const useMovieFilteredDetails = (selectedMovieID: number | null) => {
-  const setSelectedMovieFilteredDetails = useMovieDBStore((state) => state.setSelectedMovieFilteredDetails);
+// Custom hook for movie details with type guards
+export const useMovieFilteredDetails = () => {
+  const [movieFilteredDetails, setMovieFilteredDetails] = useState<FilteredDetails>({ ...initialFilteredDetails });
+  const { movieID } = useParams();
+
+  const isValidMovieID = (id: string | undefined): id is string => {
+    return id !== undefined && !isNaN(Number(id));
+  };
 
   const fetchSelectedMovieDetails = useCallback(async () => {
-    if (!selectedMovieID) return;
+    if (!isValidMovieID(movieID)) {
+      console.warn('Invalid movie ID:', movieID);
+      return;
+    }
+
+    const numericMovieID = Number(movieID);
+
     try {
-      const data: FilteredDetails = await fetchFilteredMovieDetails(selectedMovieID);
-      setSelectedMovieFilteredDetails(data);
+      const data = await fetchFilteredMovieDetails(numericMovieID);
+
+      if (typeof data === 'object' && data !== null) {
+        setMovieFilteredDetails(data);
+      } else {
+        console.error('Invalid data structure received from API:', data);
+      }
     } catch (error) {
       console.error('Error fetching selected movie details:', error);
     }
-  }, [selectedMovieID, setSelectedMovieFilteredDetails])
+  }, [movieID]);
 
   useEffect(() => {
     fetchSelectedMovieDetails();
   }, [fetchSelectedMovieDetails]);
+
+  return {
+    movieFilteredDetails
+  };
 };
