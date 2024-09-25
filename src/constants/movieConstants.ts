d6d65ed6cb090d2799301d33eadd74b93297c7c9
Paulo Lopez <geekcrasher@gmail.com>
2024-09-25 13:56:59 +0800

    refactor: add new  properties

--- src/constants/movieConstants.ts
+++ src/constants/movieConstants.ts
@@ -1,5 +1,42 @@
 import { FilteredDetails } from "@/lib/types";
 
+// export const initialFilteredDetails: FilteredDetails = {
+//   backdrop_path: '',
+//   credits: {
+//     cast: [],
+//   },
+//   genres: [{
+//     id: null,
+//     name: ''
+//   }],
+//   homepage: undefined,
+//   id: null,
+//   title: '',
+//   overview: '',
+//   popularity: 0,
+//   poster_path: '',
+//   recommendations: {
+//     results: []
+//   },
+//   release_date: '',
+//   revenue: 0,
+//   reviews: {
+//     results: []
+//   },
+//   runtime: 0,
+//   videos: {
+//     results: [],
+//   },
+//   vote_average: 0,
+//   vote_count: 0,
+//   production_companies: [],
+//   production_countries: [],
+//   status: "",
+//   tagline: "",
+//   spoken_languages: []
+// };
+
+
 export const initialFilteredDetails: FilteredDetails = {
   backdrop_path: '',
   credits: {
@@ -11,10 +48,11 @@ export const initialFilteredDetails: FilteredDetails = {
   }],
   homepage: undefined,
   id: null,
-  title: '',
   overview: '',
   popularity: 0,
   poster_path: '',
+  production_companies: [],
+  production_countries: [],
   recommendations: {
     results: []
   },
@@ -24,9 +62,14 @@ export const initialFilteredDetails: FilteredDetails = {
     results: []
   },
   runtime: 0,
+  spoken_languages: [],
+  status: "",
+  tagline: "",
+  title: '',
+  vote_average: 0,
+  vote_count: 0,
   videos: {
     results: [],
   },
-  vote_average: 0,
-  vote_count: 0,
 };
+
