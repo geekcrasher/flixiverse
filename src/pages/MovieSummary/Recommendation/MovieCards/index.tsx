e15a2ab18682b1f42a248afefa27405ebbcca0b8
Paulo Lopez <geekcrasher@gmail.com>
2024-09-25 23:47:25 +0800

    change the layout of the movie card

--- src/pages/MovieSummary/Recommendation/MovieCards/index.tsx
+++ src/pages/MovieSummary/Recommendation/MovieCards/index.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { useMovieDB } from "@/hooks/useMovieDB";
 import { MovieCollections } from "@/lib/types";
 import { Card, CardContent } from "@/components/ui/card";
+import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 
 
 type MovieCardsProp = {
@@ -17,27 +18,36 @@ const MovieCards = ({ movie }: MovieCardsProp) => {
 
   return (
     <Card
-      className="border-0 cursor-pointer overflow-hidden"
+      className="border-0 cursor-pointer overflow-hidden bg-transparent"
       onClick={() => {
         navigate(`/watch/${id}?title=${title.toLocaleLowerCase()}`)
         setSelectedMovieID(id)
       }}
     >
       <CardContent className="p-0">
-        <figure className=" flex items-center justify-center h-40 sm:h-56 w-[106.66px] sm:w-[138.66px]"
-          style={{
-            background: `url(https://image.tmdb.org/t/p/w500/${poster_path})`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            backgroundColor: `${!poster_path && '#374151'}`
-          }}
-        >
-          {!poster_path && (
-            <h2 className="font-bold flex text-turquoise [font-size:clamp(1.25rem,2.5vw,1.5rem)]">
-              flixi
-              <span className="text-[#797D8B]">Verse</span>
-            </h2>)}
+        <figure className="w-28 sm:w-36 h-auto space-y-5">
+          <AspectRatio
+            ratio={4 / 6}
+            className={`flex items-center justify-center rounded-lg overflow-hidden ${!poster_path ? 'bg-[#374151]' : null}`}
+          >
+            {
+              poster_path ? (
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+                  alt={`${title} poster path`}
+                  className="w-full h-full object-cover"
+                />
+              ) : (
+                <h2 className="font-bold flex text-turquoise [font-size:clamp(1.25rem,2.5vw,1.5rem)]">
+                  flixi
+                  <span className="text-[#797D8B]">Verse</span>
+                </h2>
+              )
+            }
+          </AspectRatio>
+          <figcaption className="truncate font-medium text-sm sm:text-base text-gray-400">
+            {title}
+          </figcaption>
         </figure>
       </CardContent>
     </Card>
