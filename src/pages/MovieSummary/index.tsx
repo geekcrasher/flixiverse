0a3534a12dc70e32e20eeddc6cd430c8889f0377
Paulo Lopez <geekcrasher@gmail.com>
2024-09-27 23:50:49 +0800

    refactor: utilize the resuable components from component folder

--- src/pages/MovieSummary/index.tsx
+++ src/pages/MovieSummary/index.tsx
@@ -1,23 +1,21 @@
 import { useMemo } from "react";
-import { useNavigate } from 'react-router-dom';
 import { useMovieFilteredDetails } from "@/hooks/useMovieFilteredDetails";
 import { useMovieStore } from "@/store/useMovieStore";
 import { debounce } from "@/utils/debounce";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { AspectRatio } from "@/components/ui/aspect-ratio";
 import Heading from "@/components/Heading";
 import Recommendation from "./Recommendation";
 import Reviews from "./Reviews";
 import Cast from "./Cast";
-import ShareContent from "./ShareContent";
-import MovieActions from "./MovieActions";
-import { ArrowLeft, Bookmark, Star } from "lucide-react";
+import BackdropCard from "@/components/Cards/BackdropCard";
 import MovieMetaData from "./MovieMetaData";
+import Share from "@/components/Dialog/Share";
+import TrailerAction from "@/components/Dialog/TrailerAction";
+import BackButton from "@/components/Button/BackButton";
+import { Bookmark, Star } from "lucide-react";
 
 const MovieSummary = () => {
-
-  const navigate = useNavigate()
 
   const movieAddedToWishlist = useMovieStore((state) => state.movieAddedToWishlist)
   const movieAddedToBookmark = useMovieStore((state) => state.movieAddedToBookmark)
@@ -31,7 +29,7 @@ const MovieSummary = () => {
   const { movieFilteredDetails } = useMovieFilteredDetails()
 
   const { id, backdrop_path, title, overview,
-    release_date, runtime, vote_average, vote_count } = movieFilteredDetails
+    release_date, runtime, vote_average, vote_count, videos } = movieFilteredDetails
 
   const formattedDuration = useMemo(() => {
     const hours = Math.floor(runtime / 60);
@@ -63,7 +61,8 @@ const MovieSummary = () => {
     <section className="relative w-auto">
       <section className="w-full flex xl:hidden items-center justify-center">
         <section className="fixed z-50 bottom-0 w-[98vw] lg:w-[89vw] py-6 bg-neutral/30 backdrop-blur-sm">
-          <MovieActions
+          <TrailerAction
+            videos={videos}
             debouncedWishlistClick={debouncedWishlistClick}
             isMovieAddedToWishlist={isMovieAddedToWishlist}
             className="w-full"
@@ -71,33 +70,14 @@ const MovieSummary = () => {
         </section>
       </section>
       <section className="pt-4 pb-32 space-y-6 px-4 md:px-6 xl:px-10 xl:pb-14">
-        <Button
-          className="flex items-center gap-2 text-sm mt-6 rounded-lg p-2 bg-gray-700 hover:bg-gray-700/70 text-gray-300 hover:text-turquoise"
-          onClick={() => navigate(-1)}
-        >
-          <ArrowLeft className="h-4 w-4" /> Back
-        </Button>
+        <BackButton />
         <section className="flex flex-col">
           <section className="grid space-y-10 xl:space-y-0 xl:grid-cols-5 xl:gap-14">
-            <figure className="xl:col-span-3 flex xl:max-w-full rounded-lg overflow-hidden space-y-6 ">
-              <AspectRatio ratio={16 / 9} className="flex items-center justify-center bg-gray-700">
-                {
-                  backdrop_path ? (
-                    <img
-                      src={`https://image.tmdb.org/t/p/w1280/${backdrop_path}`}
-                      alt={`${title} backdrop image`}
-                      className="h-full w-full object-cover"
-                    />
-                  ) : (
-                    <h2 className="font-bold flex text-turquoise [font-size:clamp(1.5rem,2.5vw,2.25rem)] ">
-                      flixi
-                      <span className="text-[#797D8B]">Verse</span>
-                    </h2>
-                  )
-                }
-              </AspectRatio>
-            </figure>
-            <section className="flex flex-col gap-6 xl:col-span-2">
+            <BackdropCard
+              backdrop_path={backdrop_path}
+              title={title}
+            />
+            <section className="flex flex-col gap-10 xl:col-span-2">
               <section className="flex items-center justify-between">
                 <p className="flex items-center gap-2 text-gray-200 font-medium">
                   <Star color="#FFC30E" fill="#FFC30E" className="size-6" />
@@ -105,7 +85,7 @@ const MovieSummary = () => {
                   <span className="font-normal text-sm text-gray-400"> | {vote_count}</span>
                 </p>
                 <section className="space-x-6">
-                  <ShareContent />
+                  <Share />
                   <Button
                     onClick={debouncedBookmarkClick}
                     className="bg-transparent hover:bg-transparent p-0"
@@ -137,14 +117,13 @@ const MovieSummary = () => {
                   </section>
                 </section>
               </section>
-              <section className="space-y-10">
-                <section className="space-y-4">
-                  <Heading title="Overview" />
-                  <p className="text-gray-400 text-base line-clamp-6">{overview}</p>
-                </section>
+              <section className="space-y-4">
+                <Heading title="Overview" />
+                <p className="text-gray-400 text-base line-clamp-6">{overview}</p>
               </section>
 
-              <MovieActions
+              <TrailerAction
+                videos={videos}
                 debouncedWishlistClick={debouncedWishlistClick}
                 isMovieAddedToWishlist={isMovieAddedToWishlist}
                 className="hidden xl:flex"
