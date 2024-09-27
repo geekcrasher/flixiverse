e0f06d86df9b1cabaa0b4d4ecd92ae31b7923677
Paulo Lopez <geekcrasher@gmail.com>
2024-09-27 23:32:40 +0800

    add new contant object

--- src/constants/movieConstants.ts
+++ src/constants/movieConstants.ts
@@ -1,43 +1,11 @@
-import { FilteredDetails, MovieList } from "@/lib/types";
+import {
+  type MovieFilteredDetails,
+  type MovieList,
+  type TVShowFilteredDetails,
+  type LastEpisodeToAir,
+} from "@/lib/types";
 
-// export const initialFilteredDetails: FilteredDetails = {
-//   backdrop_path: '',
-//   credits: {
-//     cast: [],
-//   },
-//   genres: [{
-//     id: null,
-//     name: ''
-//   }],
-//   homepage: undefined,
-//   id: null,
-//   title: '',
-//   overview: '',
-//   popularity: 0,
-//   poster_path: '',
-//   recommendations: {
-//     results: []
-//   },
-//   release_date: '',
-//   revenue: 0,
-//   reviews: {
-//     results: []
-//   },
-//   runtime: 0,
-//   videos: {
-//     results: [],
-//   },
-//   vote_average: 0,
-//   vote_count: 0,
-//   production_companies: [],
-//   production_countries: [],
-//   status: "",
-//   tagline: "",
-//   spoken_languages: []
-// };
-
-
-export const initialFilteredDetails: FilteredDetails = {
+export const initialFilteredMovieDetails: MovieFilteredDetails = {
   backdrop_path: '',
   credits: {
     cast: [],
@@ -72,6 +40,67 @@ export const initialFilteredDetails: FilteredDetails = {
     results: [],
   },
 };
+
+const lastEpisode: LastEpisodeToAir = {
+  id: 0,
+  name: '',
+  overview: '',
+  vote_average: 0,
+  vote_count: 0,
+  air_date: '',
+  episode_number: 0,
+  episode_type: '',
+  production_code: '',
+  runtime: null,
+  season_number: 0,
+  show_id: 0,
+  still_path: null,
+}
+
+export const initialFilteredTVShowDetails: TVShowFilteredDetails = {
+  adult: false,
+  backdrop_path: '',
+  created_by: [],
+  episode_run_time: [],
+  first_air_date: '',
+  genres: [],
+  homepage: '',
+  id: 0,
+  in_production: false,
+  languages: [],
+  last_air_date: '',
+  last_episode_to_air: lastEpisode,
+  name: '',
+  next_episode_to_air: null,
+  networks: [],
+  number_of_episodes: 0,
+  number_of_seasons: 0,
+  origin_country: [],
+  original_language: '',
+  original_name: '',
+  overview: '',
+  popularity: 0,
+  poster_path: '',
+  production_companies: [],
+  production_countries: [],
+  recommendations: {
+    results: []
+  },
+  reviews: {
+    results: []
+  },
+  seasons: [],
+  spoken_languages: [],
+  status: '',
+  tagline: '',
+  type: '',
+  videos: {
+    results: []
+  },
+  vote_average: 0,
+  vote_count: 0
+};
+
 
 export const initialMovieListDetails: MovieList = {
   page: 0,
